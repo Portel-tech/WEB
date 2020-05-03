c7fdfca4be0957820ef56e24a4613ee9e581c928
Mohammed Thasheel P K <thasheelpk@gmail.com>
2020-05-03 12:17:04 +0530

    button change updated

---
+++ main.js
@@ -0,0 +1,33 @@
+function endCall()
+{
+
+}
+
+function change_chatBtn()
+{
+    var elem = document.getElementById("chatBtn");
+    if (elem.value=="dis") elem.data= "assets/buttons/Chat-enabled-Button.svg",elem.value="ena";
+    else elem.data= "assets/buttons/Chat-disabled-Button.svg",elem.value="dis";
+    //elem.classList.add("btn_dis");
+}
+
+function change_videoBtn()
+{
+	var elem = document.getElementById("videoBtn");
+    if (elem.value=="dis") elem.data= "assets/buttons/Video-enabled-Button.svg",elem.value="ena";
+    else elem.data= "assets/buttons/Video-disabled-Button.svg",elem.value="dis";
+}
+
+function change_micBtn()
+{
+	var elem = document.getElementById("micBtn");
+    if (elem.value=="dis") elem.data= "assets/buttons/Mic-enabled-Button.svg",elem.value="ena";
+    else elem.data= "assets/buttons/Mic-disabled-Button.svg",elem.value="dis";	
+}
+
+function change_screenShareBtn()
+{
+	var elem = document.getElementById("screenShareBtn");
+    if (elem.value=="dis") elem.data= "assets/buttons/Screenshare-enabled-Button.svg",elem.value="ena";
+    else elem.data= "assets/buttons/Screenshare-disabled-Button.svg",elem.value="dis";	
+}
